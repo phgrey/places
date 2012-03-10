c378b968c5fe920b6739498a5915588b6e597c1f
phgrey <phgrey@gmail.com>
2012-03-10 16:47:56 +0200

    db localizations - model for texts

---
+++ app/models/text.rb
@@ -0,0 +1,14 @@
+class Text < ActiveRecord::Base
+  validates :lang, :item_type,  :presence => true
+  #validates :text, :lang, :item_id, :item_type,  :presence => true
+  validates :text, :length => {:in => 2..500, :allow_blank => true }
+  validates :lang, :inclusion => { :in => %w(ru en)}
+  validates :item_type, :inclusion => {:in => %w(Offer Task)}
+
+  def self.new_for_each_locale(item_type)
+    [
+        new(:lang => 'ru', :item_type => item_type),
+        new(:lang => 'en', :item_type => item_type),
+    ]
+  end
+end
