23503d5e74a08e505f0c51a2919394fbf922fde5
phgrey <phgrey@gmail.com>
2012-04-15 02:18:12 +0300

    some missed schema modifications while adding profile data to users

---
+++ db/migrate/20120313214204_add_profile_to_users.rb
@@ -0,0 +1,12 @@
+class AddProfileToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :lang, :string, :default => 'en', :null => false
+
+    add_column :users, :photo, :string
+
+    add_column :users, :habred, :boolean, :default => 0, :null => false
+
+    add_column :users, :options, :int, :default => 1
+
+  end
+end
